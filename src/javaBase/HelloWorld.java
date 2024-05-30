7b3cacb886ec2633e1dba275cd6cf0d2057a154d
Trungtqz <trungto27082001@gmail.com>
2024-05-30 16:54:42 +0700

    init project

---
+++ src/javaBase/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaBase;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and Trungtqv");
+		System.out.println("Hello world with java and Trungtqv");
+		System.out.println("Hello world with java and Trungtqv");
+		System.out.println("Hello world with java and Trungtqv");
+		System.out.println("Hello world with java and Trungtqv");
+	}
+}
